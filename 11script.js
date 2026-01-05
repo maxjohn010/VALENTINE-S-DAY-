ca3edac59b02dca647c46f0c915e431658f5a33a
maxjohn010 <shaizgill295@gmail.com>
2026-01-05 18:13:20 +0500

    Add files via upload

---
+++ 11script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??🤨",
+    "Are you positive?🧐",
+    "Jana g please...🤗",
+    "Just think about it!☹️",
+    "If you say no, I will be really sad...😮",
+    "I will be very sad...🥺",
+    "I will be very very very sad...😭",
+    "Ok fine, I will stop asking...🙂",
+    "Just kidding, say yes please!💝"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+  const noButton = document.querySelector('.no-button');
+  const yesButton = document.querySelector('.yes-button');
+  noButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+  const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+  window.location.href = "yes_page.html";
+}
